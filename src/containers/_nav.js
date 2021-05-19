012b6912e8e2cc907dbe14716cc30569134abe7e
Gayath1 <chandulagayan@gmail.com>
2021-05-19 16:18:08 +0530

    changes nav bar

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -122,12 +122,6 @@ const _nav =  [
       },
       {
         _tag: 'CSidebarNavItem',
-        name: 'Employee Dashboard',
-        to: '/Leave Configuration',
-        icon: 'cil-calculator',
-      },
-      {
-        _tag: 'CSidebarNavItem',
         name: 'Leave Application(employee)',
         to: '/Leave Application',
         icon: 'cil-cursor',
@@ -135,92 +129,6 @@ const _nav =  [
     ],
   },
 
-  {
-    _tag: 'CSidebarNavDivider'
-  },
-  {
-    _tag: 'CSidebarNavTitle',
-    _children: ['Employee'],
-  },
-  {
-    _tag: 'CSidebarNavDropdown',
-    name: 'Temp',
-    route: '/pages',
-    icon: 'cil-star',
-    _children: [
-      {
-        _tag: 'CSidebarNavItem',
-        name: 'Employee Login',
-        to: '/Employee Login',
-      },
-      {
-        _tag: 'CSidebarNavItem',
-        name: 'Employee Home',
-        to: '/Employee Home',
-      },
-      {
-        _tag: 'CSidebarNavItem',
-        name: 'Change Password',
-        to: '/Change Password',
-      },
-      {
-        _tag: 'CSidebarNavItem',
-        name: 'Settings',
-        to: '/Settings',
-      },
-    ],
-  },
-
-  {
-    _tag: 'CSidebarNavDivider',
-    className: 'm-2'
-  }, {
-    _tag: 'CSidebarNavTitle',
-    _children: ['Extras'],
-  },
-  {
-    _tag: 'CSidebarNavDropdown',
-    name: 'Pages',
-    route: '/pages',
-    icon: 'cil-star',
-    _children: [
-      {
-        _tag: 'CSidebarNavItem',
-        name: 'Login',
-        to: '/login',
-      },
-      {
-        _tag: 'CSidebarNavItem',
-        name: 'Register',
-        to: '/register',
-      },
-      {
-        _tag: 'CSidebarNavItem',
-        name: 'Error 404',
-        to: '/404',
-      },
-      {
-        _tag: 'CSidebarNavItem',
-        name: 'Error 500',
-        to: '/500',
-      },
-    ],
-  },
-  {
-    _tag: 'CSidebarNavItem',
-    name: 'Disabled',
-    icon: 'cil-ban',
-    badge: {
-      color: 'secondary',
-      text: 'NEW',
-    },
-    addLinkClass: 'c-disabled',
-    'disabled': true
-  },
-  {
-    _tag: 'CSidebarNavDivider',
-    className: 'm-2'
-  },
 
 ]
 
