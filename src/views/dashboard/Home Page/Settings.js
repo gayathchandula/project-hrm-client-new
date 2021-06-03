540bbc75fff3bbdfee224964f71e388b123b12fc
Gayath1 <chandulagayan@gmail.com>
2021-06-03 13:35:15 +0530

    settings unused remove

--- src/views/dashboard/Home Page/Settings.js
+++ src/views/dashboard/Home Page/Settings.js
@@ -1,33 +1,28 @@
 import React, {useEffect, useState} from 'react'
 import {
   CCol,
-  CNav,
-  CNavItem,
-  CNavLink,
+
   CRow,
-  CTabContent,
-  CTabPane,
+
   CCard,
   CCardBody,
   CButton,
-  CTabs,
+
   CForm,
   CFormGroup,
   CLabel,
   CInput,
-  CFormText,
-  CCardFooter,
+
 
 
   CCardHeader, CSpinner
 } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { DocsLink } from 'src/reusable'
+
 import {Link} from "react-router-dom";
 import axios from "axios";
 var imageName = require('src/assets/img_avatar.png')
 const Tabs = () => {
-  const [active, setActive] = useState(1)
+
   const [listData, setListData] = useState({ lists: [] });
   const [loading, setLoading] = useState(true);
 
@@ -51,7 +46,7 @@ const Tabs = () => {
     };
     fetchData();
   }, []);
-  const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.'
+
   if (loading) {
     return (
       <div style={{ padding: "10px 20px", textAlign: "center"}}>
